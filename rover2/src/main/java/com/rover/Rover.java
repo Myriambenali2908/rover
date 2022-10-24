a74d84b1b159621dd12b3d19eeeee7175be09b58
Myriambenali2908 <116118197+Myriambenali2908@users.noreply.github.com>
2022-10-24 10:12:43 +0200

    Update Rover.java

--- rover2/src/main/java/com/rover/Rover.java
+++ rover2/src/main/java/com/rover/Rover.java
@@ -12,39 +12,40 @@ public class Rover {
         String line, line1, line2;
         String[] roverPosition;
         String instructions;
-        int x ;
+        int x;
         int y;
         int limitX = 0;
         int limitY = 0;
         try {
             input = new BufferedReader(new FileReader(argv[0]));
+            if ((line = input.readLine()) != null) {
+                String[] s = line.split(" ");
+                limitX = Integer.parseInt(s[0]);
+                limitY = Integer.parseInt(s[1]);
+            }
+            while ((line1 = input.readLine()) != null
+                    && (line2 = input.readLine()) != null) {
+                roverPosition = line1.split(" ");
+                x = Integer.parseInt(roverPosition[0]);
+                y = Integer.parseInt(roverPosition[1]);
+                instructions = line2;
+                String direction = roverPosition[2];
+                for (int i = 0; i < instructions.length(); i++) {
+                    if (instructions.charAt(i) != 'M') {
+                        direction = getNextDirection(direction, instructions.charAt(i));
+                    } else if (instructions.charAt(i) == 'M') {
+                        int[] pos = getGridPosition(direction, x, y, limitX, limitY);
+                        x = pos[0];
+                        y = pos[1];
+                    }
+                }
+                System.out.println(x + " " + y + " " + direction);
+            }
         } catch (FileNotFoundException exc) {
             System.out.println("Erreur d'ouverture du fichier");
+        } finally {
+            input.close();
         }
-        if ((line = input.readLine()) != null) {
-            String[] s = line.split(" ");
-            limitX = Integer.parseInt(s[0]);
-            limitY = Integer.parseInt(s[1]);
-        }
-        while ((line1 = input.readLine()) != null
-                && (line2 = input.readLine()) != null) {
-            roverPosition = line1.split(" ");
-            x = Integer.parseInt(roverPosition[0]);
-            y = Integer.parseInt(roverPosition[1]);
-            instructions = line2;
-            String direction = roverPosition[2];
-            for (int i = 0; i < instructions.length(); i++) {
-                if (instructions.charAt(i) != 'M') {
-                    direction = getNextDirection(direction, instructions.charAt(i));
-                } else if (instructions.charAt(i) == 'M') {
-                    int[] pos = getGridPosition(direction, x, y, limitX, limitY);
-                    x = pos[0];
-                    y = pos[1];
-                }
-            }
-            System.out.println(x + " " + y + " " + direction);
-        }
-        input.close();
     }
 
     private static String getNextDirection(String direction, char rotation) {
@@ -66,7 +67,7 @@ public class Rover {
             case "E":
                 return new int[]{x + 1 <= limitX ? x + 1 : x, y};
             case "S":
-                return new int[]{x, y -1 >= 0 ? y - 1 : y};
+                return new int[]{x, y - 1 >= 0 ? y - 1 : y};
             case "W":
                 return new int[]{x - 1 >= 0 ? x - 1 : x, y};
             default:
